e089fb636773fcd1d5154792e8b627b7fd9ca24c
Vlad Vladimirov <blad20002000@mail.ru>
2022-02-10 14:05:56 +0700

    any

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const btn = document.querySelector('#e_btn')
-const circle = document.querySelector('#circle')
-const square = document.querySelector('#square')
+'use strict'
 
-const toggler = function (event) {
-    console.log(event.target.id + "  " + event.currentTarget.id);
-    event.target.classList.toggle('green')
+const btn = document.getElementById('btn')
+const range = document.getElementById('range')
+const span = document.getElementById('range-span')
+const input = document.getElementById('text')
+const square = document.getElementById('square')
+const circle = document.getElementById('circle')
+const e_btn = document.getElementById('e_btn')
+
+e_btn.style.display = "none"
+
+const squareColor = function () {
+    square.style.backgroundColor = input.value
 }
 
-btn.addEventListener('click', toggler, true)
-circle.addEventListener('click', toggler, true)
-square.addEventListener('click', toggler, true)
+const circleSize = function () {
+    span.textContent = range.value
+    circle.style.height = range.value + "%"
+    circle.style.width = range.value + "%"
+}
+
+btn.addEventListener('click', squareColor)
+range.addEventListener('input', circleSize)
